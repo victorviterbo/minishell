b9d424059a14b8525865fc31ae5c41d690d71309
vviterbo <victor.viterbo@epfl.ch>
2025-01-29 12:27:04 +0100

    added tests for pwd and oldpwd updating in cd

--- tests/test_cd_pwd.c
+++ tests/test_cd_pwd.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:02:46 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/02/06 19:58:18 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/02/06 20:11:56 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ char	**ft_make_test_strarr(char *str);
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	*here;
+	char	*pwd_var;
+	char	*oldpwd_var;
+	char	*curr_path;
 	t_data	*data;
 	pid_t	pid;
 
@@ -27,8 +30,24 @@ int	main(int argc, char *argv[], char *envp[])
 	init_env(data, envp);
 	if (ft_pwd() != 0)
 		return (EXIT_FAILURE);
+	pwd_var = get_var(data, "PWD");
+	curr_path = ft_get_current_path();
+	if (ft_strcmp(pwd_var, curr_path))
+		return (EXIT_FAILURE);
 	if (ft_cd(data, "testdir") != 0)
 		return (EXIT_FAILURE);
+	oldpwd_var = get_var(data, "OLDPWD");
+	if (ft_strcmp(pwd_var, oldpwd_var))
+		return (EXIT_FAILURE);
+	free(oldpwd_var);
+	free(pwd_var);
+	free(curr_path);
+	pwd_var = get_var(data, "PWD");
+	curr_path = ft_get_current_path();
+	if (ft_strcmp(pwd_var, curr_path))
+		return (EXIT_FAILURE);
+	free(pwd_var);
+	free(curr_path);
 	if (ft_pwd() != 0)
 		return (EXIT_FAILURE);
 	ft_cd(data, here);
