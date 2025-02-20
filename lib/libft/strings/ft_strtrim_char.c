dd6da3999c423fceb3a7a163af208616013f9f1a
vviterbo <victor.viterbo@epfl.ch>
2025-02-20 14:10:19 +0100

    WIP parsing tree building

--- lib/libft/strings/ft_strtrim_char.c
+++ lib/libft/strings/ft_strtrim_char.c
@@ -6,15 +6,15 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 23:22:58 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/02/14 17:59:06 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/02/16 17:45:25 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim_char(char	*str, char c, bool inplace);
+char	*ft_strtrim_char(char *str, char c, bool inplace);
 
-char	*ft_strtrim_char(char	*str, char c, bool inplace)
+char	*ft_strtrim_char(char *str, char c, bool inplace)
 {
 	char	*cstr;
 	char	*trimmed;
--- srcs/parsing/build_tree.c
+++ srcs/parsing/build_tree.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 11:47:44 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/02/16 17:30:55 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/02/16 18:20:00 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 void	build_tree(char *str, t_tree *tree);
 bool	is_cmd_separator(char c1, char c2);
 void	create_node(t_tree *tree, char *str, size_t i);
+char	*parse_parenthesis(char *str, bool inplace);
 
 void	build_tree(char *str, t_tree *tree)
 {
 	int		*isescaped;
 	size_t	i;
 
+	str = remove_parenthesis(str);
 	isescaped = is_quote_escaped(str);
 	i = -1;
 	while (str[i + 1])
 	{
 		i++;
-		if (str[i] == '(')
-		{
-			i = go_to_next(str, ")", i);
-			continue ;
-		}
-		else if (!is_cmd_separator(str[i], str[i + 1]) | isescaped[i])
+		if (!is_cmd_separator(str[i], str[i + 1]) | isescaped[i])
 			continue ;
 		else
 			create_node(tree, str, i);
@@ -46,12 +43,24 @@ bool	is_cmd_separator(char c1, char c2)
 		return (true);
 	else if (c1 == '&' && c2 == '&')
 		return (true);
+	else if (c1 == '(')
+		return (true);
 	return (false);
 }
 
 void	create_node(t_tree *tree, char *str, size_t i)
 {
-	if (str[i] == '|' && str[i+ 1] == '|')
+	size_t	j;
+
+	if (str[i] == '(')
+	{
+		tree->content = ft_strdup("()");
+		j = go_to_next(str, ")", i);
+		build_tree(ft_substr(str, 0, j), tree->left);
+		build_tree(ft_substr(str, j, ft_strlen(str + j)), tree->right);
+		return ;
+	}
+	else if (str[i] == '|' && str[i+ 1] == '|')
 		tree->content = ft_strdup ("||");
 	else if (str[i] == '&' && str[i + 1] == '&')
 		tree->content = ft_strdup ("&&");
